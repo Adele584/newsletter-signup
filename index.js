f757c1acf1cb8d142596136ad89a70f13a6f8120
Adele584 <117003045+Adele584@users.noreply.github.com>
2023-07-20 05:15:43 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+function validateEmail() {
+  const emailCheck = document.getElementById('emailInput');
+  const emailError = document.getElementById('emailError');
+
+  if (!emailCheck.checkValidity()) {
+    emailError.textContent = "Valid email required";
+  } else {
+    emailError.textContent = "";
+  }
+}
+
+let uerEmail = document.getElementById('emailInput');
+let emailParams = new URLSearchParams(window.location.search);
+let email = emailParams.get('Email');
+let decodedEmail = decodeURIComponent(email);
+userEmail.textContent = decodedEmail;
+
+
+const Dismissing = document.getElementById('Dismissing');
+Dismissing.addEventListener('click', function() {
+  window.location.href = 'index.html';
+});
